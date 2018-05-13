3cd1017c1ff2fe2ccb56f3842a4e27d51add6481
swich <28.swich@gmail.com>
2018-05-13 18:00:17 +0300

    Add todo command

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const token = '341606823:AAHRo7IA-7rRVI2B-PuDa4jTSaHrRVVvV6U';
 
 const bot = new TelegramBot(token, {polling: true});
 
+var chatId;
+
+bot.onText(/(.+)/, (msg) =>{
+	chatId = msg.chat.id;
+})
+
 // Start
 bot.onText(/\/start/, (msg) => {
-	const chatId = msg.chat.id;
-
 	const resp = "Welcome to YDLS Bot!";
 	bot.sendMessage(chatId, resp);
 });
@@ -15,16 +19,24 @@ bot.onText(/\/start/, (msg) => {
 
 // Echo
 bot.onText(/\/echo (.+)/, (msg, match) => {
-	const chatId = msg.chat.id;
-
 	const resp = match[1];
 	bot.sendMessage(chatId, resp);
 });
 
 // Test
 bot.onText(/\/test/, (msg) => {
-	const chatId = msg.chat.id;
-
-	const resp = "OK!";
+	const resp = "It's Work!";
 	bot.sendMessage(chatId, resp);
 });
+
+// Text file
+var fs = require('fs');
+
+bot.onText(/\/todo/, (msg) => {
+	fs.readFile('todo.txt', 'utf8', function(err, data){
+		if (err) throw err;
+		const resp = 'To Do List:\n' + data;
+		bot.sendMessage(chatId, resp);
+	})
+});
+
